a2d413d8e2e73f66a8830494e53d70dd657baddf
JiangWenqi <jiangwenqi1995@gmail.com>
2022-03-03 23:58:23 +0100

    add output file with NT format

--- sparql_based-linking/src/main/java/upm/oeg/loom/examples/SimilarityFunctionExample.java
+++ sparql_based-linking/src/main/java/upm/oeg/loom/examples/SimilarityFunctionExample.java
@@ -52,6 +52,7 @@ public class SimilarityFunctionExample {
             + "PREFIX wdt: <http://www.wikidata.org/prop/direct/>"
             + "PREFIX dbo: <http://dbpedia.org/ontology/> "
             + "PREFIX dbr: <http://dbpedia.org/resource/> "
+            + "PREFIX loom:    <http://oeg.upm.es/loom-ld/functions/link#>"
             + "SELECT * WHERE {"
             + "  SERVICE <https://query.wikidata.org/sparql> {"
             + "    SELECT ?work1 WHERE {"
@@ -63,14 +64,17 @@ public class SimilarityFunctionExample {
             + "      ?work2 dbo:author dbr:Johannes_V._Jensen ."
             + "    }"
             + "  }"
+            + "BIND(loom:levenshtein(?work1, ?work2) AS ?levenshtein)"
+            + "BIND(loom:cosine(?work1, ?work2) AS ?cosine)"
             + "}";
     Query query = QueryFactory.create(sparqlString);
     try (QueryExecution qexec =
         QueryExecutionFactory.create(query, ModelFactory.createDefaultModel())) {
       ResultSet rs = qexec.execSelect();
 
+
+//            ResultSetFormatter.out(System.out, rs, query);
       OutputStream outputStream = new FileOutputStream("./test.rdf");
-      //      ResultSetFormatter.out(System.out, rs, query);
       ResultSetFormatter.output(outputStream, rs, ResultsFormat.FMT_RDF_NT);
     } catch (FileNotFoundException e) {
       e.printStackTrace();
