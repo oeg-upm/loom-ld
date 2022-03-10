b6665bcbb60a8eb5774c76beacacb5dd2a844a77
JiangWenqi <jiangwenqi1995@gmail.com>
2022-03-10 14:14:27 +0100

    built an example for basic construct similarity query

--- sparql_based-linking/src/main/java/upm/oeg/loom/examples/SimilarityFunctionExample.java
+++ sparql_based-linking/src/main/java/upm/oeg/loom/examples/SimilarityFunctionExample.java
@@ -1,13 +1,12 @@
 package upm.oeg.loom.examples;
 
-import org.apache.jena.query.*;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.sparql.resultset.ResultsFormat;
 import upm.oeg.loom.functions.CustomFunctions;
-
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
 
 /**
  * This example is for test loom-ld similarity functions
@@ -20,64 +19,35 @@ public class SimilarityFunctionExample {
   }
 
   public static void main(String[] args) {
-    SimilarityFunctionExample example = new SimilarityFunctionExample();
-    example.test2();
-  }
 
-  private void test() {
-
-    String queryString =
-        "PREFIX loom:    <http://oeg.upm.es/loom-ld/functions/link#>"
-            + "SELECT * WHERE { "
-            + "    SERVICE <http://dbpedia-live.openlinksw.com/sparql?timeout=2000> { "
-            + "        SELECT DISTINCT ?company where {?company a <http://dbpedia.org/ontology/Company>} LIMIT 20"
-            + "    }"
-            + "    SERVICE <http://dbpedia-live.openlinksw.com/sparql?timeout=2000> { "
-            + "        SELECT DISTINCT ?company1 where {?company1 a <http://dbpedia.org/ontology/Company>} LIMIT 20"
-            + "    }"
-            + "BIND(loom:levenshtein(?company, ?company1) AS ?grade)"
-            + "}";
-
-    Query query = QueryFactory.create(queryString);
-    try (QueryExecution qexec =
-        QueryExecutionFactory.create(query, ModelFactory.createDefaultModel())) {
-      ResultSet rs = qexec.execSelect();
-      ResultSetFormatter.out(System.out, rs, query);
-    }
-  }
-
-  private void test2() {
-    String sparqlString =
-        "PREFIX wd: <http://www.wikidata.org/entity/>"
-            + "PREFIX wdt: <http://www.wikidata.org/prop/direct/>"
-            + "PREFIX dbo: <http://dbpedia.org/ontology/> "
-            + "PREFIX dbr: <http://dbpedia.org/resource/> "
+    String sparql3 =
+        "PREFIX rdf:     <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
+            + "PREFIX owl:     <http://www.w3.org/2002/07/owl#>\n"
             + "PREFIX loom:    <http://oeg.upm.es/loom-ld/functions/link#>"
-            + "SELECT * WHERE {"
-            + "  SERVICE <https://query.wikidata.org/sparql> {"
-            + "    SELECT ?work1 WHERE {"
-            + "      ?work1 wdt:P50 wd:Q159552 ."
-            + "    }"
-            + "  }"
-            + "  SERVICE <http://DBpedia.org/sparql> {"
-            + "    SELECT ?work2 WHERE {"
-            + "      ?work2 dbo:author dbr:Johannes_V._Jensen ."
-            + "    }"
-            + "  }"
-            + "BIND(loom:levenshtein(?work1, ?work2) AS ?levenshtein)"
-            + "BIND(loom:cosine(?work1, ?work2) AS ?cosine)"
-            + "}";
-    Query query = QueryFactory.create(sparqlString);
+            + "PREFIX foaf:    <http://xmlns.com/foaf/0.1/>\n"
+            + "PREFIX dbo:    <http://dbpedia.org/ontology/>\n"
+            + "PREFIX schema: <http://schema.org/>\n"
+            + "CONSTRUCT {\n"
+            + "   ?city1 owl:sameAs ?city2 .\n"
+            + "}\n"
+            + "WHERE {\n"
+            + "   SERVICE <http://dbpedia-live.openlinksw.com/sparql?timeout=3000> { \n"
+            + "     ?city1 rdf:type schema:City.\n"
+            + "     ?city1 foaf:name ?cityName1. \n"
+            + "   }"
+            + "   SERVICE <http://dbpedia-live.openlinksw.com/sparql?timeout=3000> {\n"
+            + "     ?city2 rdf:type dbo:City.\n"
+            + "     ?city2 foaf:name ?cityName2. \n"
+            + "   }\n"
+            + "   FILTER ( loom:cosine(?cityName1, ?cityName2 ) > 0.8)\n"
+            + "}\n"
+            + "Limit 200";
+    System.out.println(sparql3);
+    Query query = QueryFactory.create(sparql3);
     try (QueryExecution qexec =
         QueryExecutionFactory.create(query, ModelFactory.createDefaultModel())) {
-      ResultSet rs = qexec.execSelect();
-
-
-//            ResultSetFormatter.out(System.out, rs, query);
-      OutputStream outputStream = new FileOutputStream("./test.rdf");
-      ResultSetFormatter.output(outputStream, rs, ResultsFormat.FMT_RDF_NT);
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
+      Model model = qexec.execConstruct();
+      model.write(System.out, "TURTLE");
     }
   }
 }
